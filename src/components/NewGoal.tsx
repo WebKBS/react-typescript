8d04d864c659c259b949cd9e87fa73ed3735eb17
gangbeomsu <kbs21c1987@gmail.com>
2024-01-12 23:03:35 +0900

    new FormData 대신 ref로 변경해서 데이터 보기

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,21 +1,31 @@
+import { useRef } from 'react';
+
 export default function NewGoal() {
+  const goal = useRef<HTMLInputElement>(null);
+  const summary = useRef<HTMLInputElement>(null);
+
   function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
 
     // new FormData(event.currentTarget).forEach((value, name) => {
     //   console.log(name + ': ' + value);
     // });
+
+    const enteredGoal = goal.current!.value;
+    const enteredSummary = summary.current!.value;
+    console.log(enteredGoal);
+    console.log(enteredSummary);
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <p>
         <label htmlFor="goal">Your goal</label>
-        <input id="goal" type="text" name="goal" />
+        <input id="goal" type="text" ref={goal} />
       </p>
       <p>
         <label htmlFor="summary">Short summary</label>
-        <input type="text" id="summary" name="summary" />
+        <input type="text" id="summary" ref={summary} />
       </p>
       <p>
         <button>Add Goal</button>
