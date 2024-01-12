77aff8fde6306e758c9ed299882930d72b76bbdc
gangbeomsu <kbs21c1987@gmail.com>
2024-01-12 22:58:39 +0900

    new FormData 로 데이터 확인하기

--- src/components/NewGoal.tsx
+++ src/components/NewGoal.tsx
@@ -1,17 +1,21 @@
 export default function NewGoal() {
-  function handleSubmit(event: React.FormEvent) {
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
+
+    // new FormData(event.currentTarget).forEach((value, name) => {
+    //   console.log(name + ': ' + value);
+    // });
   }
 
   return (
     <form onSubmit={handleSubmit}>
       <p>
         <label htmlFor="goal">Your goal</label>
-        <input id="goal" type="text" />
+        <input id="goal" type="text" name="goal" />
       </p>
       <p>
         <label htmlFor="summary">Short summary</label>
-        <input type="text" id="summary" />
+        <input type="text" id="summary" name="summary" />
       </p>
       <p>
         <button>Add Goal</button>
