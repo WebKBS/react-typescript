ee0e5cef7cf2542e4671647a67a252c008903f33
gangbeomsu <kbs21c1987@gmail.com>
2024-01-12 22:04:46 +0900

    useState 타입 정의하기

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
+import { useState } from 'react';
 import goalsImg from './assets/goals.jpg';
 import CourseGoal from './components/CourseGoal';
 import Header from './components/Header';
 
+type CourseGoal = {
+  title: string;
+  description: string;
+  id: number;
+};
+
 export default function App() {
+  const [goals, setGoals] = useState<CourseGoal[]>([]);
+  function handleAddGoal() {}
+
   return (
     <main>
       <Header image={{ src: goalsImg, alt: 'A list of goals' }}>
         <h1>Your Course Goal</h1>
       </Header>
+      <button onClick={handleAddGoal}>Add Goal</button>
       <CourseGoal title="Learn React + TS">
         <p>Learn it from the ground up</p>
       </CourseGoal>
