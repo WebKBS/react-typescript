797ce2b46db407c43d90e1795d0c8590e7d9baf9
gangbeomsu <kbs21c1987@gmail.com>
2024-01-13 15:18:52 +0900

    infobox 컴포넌트 추가

--- src/components/CourseGoalList.tsx
+++ src/components/CourseGoalList.tsx
@@ -1,6 +1,8 @@
+import React from 'react';
 import { type CourseGoal as CGoal } from '../App';
 // type 사용시 이름 중복이 있다면 as를 사용하여 이름을 변경할 수 있다.
 import CourseGoal from './CourseGoal';
+import InfoBox from './InfoBox';
 
 type CourseGoalListProps = {
   goals: CGoal[];
@@ -11,15 +13,28 @@ export default function CourseGoalList({
   goals,
   onDeleteGoal,
 }: CourseGoalListProps) {
+  if (goals.length === 0) {
+    return <InfoBox mode="hint">No goals were added yet!</InfoBox>;
+  }
+
+  let warningBox: React.ReactNode;
+
+  if (goals.length > 4) {
+    warningBox = <InfoBox mode="warning">Something went wrong!</InfoBox>;
+  }
+
   return (
-    <ul>
-      {goals.map((goal) => (
-        <li key={goal.id}>
-          <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
-            {goal.description}
-          </CourseGoal>
-        </li>
-      ))}
-    </ul>
+    <>
+      {warningBox}
+      <ul>
+        {goals.map((goal) => (
+          <li key={goal.id}>
+            <CourseGoal id={goal.id} title={goal.title} onDelete={onDeleteGoal}>
+              {goal.description}
+            </CourseGoal>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 }
