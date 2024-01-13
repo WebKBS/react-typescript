26386d2fa95611f00e0f3876ad751040ec2bf400
gangbeomsu <kbs21c1987@gmail.com>
2024-01-13 21:32:33 +0900

    timer context 완성하기

--- timer/src/components/store/timers-context.tsx
+++ timer/src/components/store/timers-context.tsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, useContext, useReducer } from 'react';
 
 type Timer = {
   name: string;
@@ -10,6 +10,11 @@ type TimersState = {
   timers: Timer[];
 };
 
+const initialState: TimersState = {
+  isRunning: true,
+  timers: [],
+};
+
 type TimersContextValue = TimersState & {
   addTimer: (timerData: Timer) => void;
   startTimers: () => void;
@@ -18,19 +23,78 @@ type TimersContextValue = TimersState & {
 
 const TimersContext = createContext<TimersContextValue | null>(null);
 
+// 커스텀 훅
+export function useTimersContext() {
+  const timersCtx = useContext(TimersContext);
+
+  if (timersCtx === null) {
+    throw new Error('TimersContext를 찾을 수 없습니다.');
+  }
+
+  return timersCtx;
+}
+
 type TimersContextProviderProps = {
   children: React.ReactNode;
 };
 
+type StartTimersAction = {
+  type: 'START_TIMERS';
+};
+
+type StopTimersAction = {
+  type: 'STOP_TIMERS';
+};
+
+type AddTimerAction = {
+  type: 'ADD_TIMER';
+  payload: Timer;
+};
+
+type Action = StartTimersAction | StopTimersAction | AddTimerAction;
+
+function timersReducer(state: TimersState, action: Action): TimersState {
+  switch (action.type) {
+    case 'START_TIMERS':
+      return {
+        ...state,
+        isRunning: true,
+      };
+    case 'STOP_TIMERS':
+      return {
+        ...state,
+        isRunning: false,
+      };
+    case 'ADD_TIMER':
+      return {
+        ...state,
+        timers: [
+          ...state.timers,
+          { name: action.payload.name, duration: action.payload.duration },
+        ],
+      };
+    default:
+      return state;
+  }
+}
+
 export default function TimersContextProvider({
   children,
 }: TimersContextProviderProps) {
+  const [timersState, dispatch] = useReducer(timersReducer, initialState);
+
   const ctx: TimersContextValue = {
-    timers: [],
-    isRunning: false,
-    addTimer: (timerData) => {},
-    startTimers: () => {},
-    stopTimers: () => {},
+    timers: timersState.timers,
+    isRunning: timersState.isRunning,
+    addTimer: (timerData) => {
+      dispatch({ type: 'ADD_TIMER', payload: timerData });
+    },
+    startTimers: () => {
+      dispatch({ type: 'START_TIMERS' });
+    },
+    stopTimers: () => {
+      dispatch({ type: 'STOP_TIMERS' });
+    },
   };
 
   return (
