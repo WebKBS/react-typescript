2d649f316463a5e61051d54f7be4ff396630a97e
gangbeomsu <kbs21c1987@gmail.com>
2024-01-13 17:58:32 +0900

    element props를 사용하지 않고 버튼 컴포넌트 타입을 만드는 방법

--- newProject/src/App.tsx
+++ newProject/src/App.tsx
@@ -5,12 +5,10 @@ function App() {
   return (
     <main>
       <p>
-        <Button element="button">A Button</Button>
+        <Button>A Button</Button>
       </p>
       <p>
-        <Button element="anchor" href="https://google.com">
-          A Link
-        </Button>
+        <Button href="https://google.com">A Link</Button>
       </p>
       <Input id="name" label="Your name" type="text" disabled />
       <Input id="age" label="Your age" type="number" />
--- newProject/src/components/Button.tsx
+++ newProject/src/components/Button.tsx
@@ -1,16 +1,18 @@
 import { ComponentPropsWithRef } from 'react';
 
-type ButtonProps = {
-  element: 'button';
-} & ComponentPropsWithRef<'button'>;
+type ButtonProps = ComponentPropsWithRef<'button'>;
 
-type AnchorProps = {
-  element: 'anchor';
-  href: string;
-} & ComponentPropsWithRef<'a'>;
+type AnchorProps = ComponentPropsWithRef<'a'>;
+
+// 아래 함수는 "href"를 가지고 있는지 확인하고 있으면 AnchorProps로 타입을 지정한다.
+function isAnchorProps(props: ButtonProps | AnchorProps): props is AnchorProps {
+  // is를 사용하는 이유는 props가 AnchorProps인지 확인하기 위해
+
+  return 'href' in props;
+}
 
 export default function Button(props: ButtonProps | AnchorProps) {
-  if (props.element === 'anchor') {
+  if (isAnchorProps(props)) {
     return <a className="button" {...props}></a>;
   }
 
