7c7ec1ffa32a7975fd3117688c96be737da6a21d
gangbeomsu <kbs21c1987@gmail.com>
2024-01-13 17:38:41 +0900

    ComponentPropsWithRef 사용해서 모든 input type 받는 방법

--- newProject/src/components/Input.tsx
+++ newProject/src/components/Input.tsx
@@ -1,13 +1,21 @@
+import { ComponentPropsWithRef } from 'react';
+
 type InputProps = {
   label: string;
   id: string;
-};
+} & ComponentPropsWithRef<'input'>; // 내장된 input의 모든 속성을 상속받는다.
 
-export default function Input({ label, id }: InputProps) {
+// 아래는 interface를 사용한 방법
+// interface InputProps extends ComponentPropsWithRef<'input'> {
+//   label: string;
+//   id: string;
+// }
+
+export default function Input({ label, id, ...props }: InputProps) {
   return (
     <p>
       <label htmlFor={id}>{label}</label>
-      <input id={id} />
+      <input id={id} {...props} />
     </p>
   );
 }
