8100347d2e1696f45dff4708499fac85f6c6e890
gangbeomsu <kbs21c1987@gmail.com>
2024-01-13 18:45:12 +0900

    form 컴포넌트 추가

--- newProject/src/App.tsx
+++ newProject/src/App.tsx
@@ -1,13 +1,21 @@
-import { useRef } from 'react';
 import Button from './components/Button';
-import Container from './components/Container';
+import Form from './components/Form';
 import Input from './components/Input';
 
 function App() {
-  const input = useRef<HTMLInputElement>(null);
+  // const input = useRef<HTMLInputElement>(null);
+
   return (
     <main>
-      <Input id="test" label="test" type="text" ref={input} />
+      <Form>
+        <Input id="name" label="Name" type="text" />
+        <Input id="age" label="Age" type="number" />
+        <p>
+          <Button>Save</Button>
+        </p>
+      </Form>
+
+      {/* <Input id="test" label="test" type="text" ref={input} />
       <Container as={Button}>Click Me</Container>
       <p>
         <Button>A Button</Button>
@@ -16,7 +24,7 @@ function App() {
         <Button href="https://google.com">A Link</Button>
       </p>
       <Input id="name" label="Your name" type="text" disabled />
-      <Input id="age" label="Your age" type="number" />
+      <Input id="age" label="Your age" type="number" /> */}
     </main>
   );
 }
