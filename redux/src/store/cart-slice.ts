4fc434ac3629a953d250419f37c5cc8abfa1ef47
gangbeomsu <kbs21c1987@gmail.com>
2024-01-14 17:26:15 +0900

    redux store slice 설정하기

---
+++ redux/src/store/cart-slice.ts
@@ -0,0 +1,45 @@
+import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+
+type CartItem = {
+  id: string;
+  title: string;
+  price: number;
+  quantity: number;
+};
+type CartState = {
+  items: CartItem[];
+};
+
+const initialState: CartState = {
+  items: [],
+};
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState: initialState,
+  reducers: {
+    addToCart(
+      state,
+      action: PayloadAction<{ id: string; title: string; price: number }>
+    ) {
+      const ItemIndex = state.items.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (ItemIndex >= 0) {
+        state.items[ItemIndex].quantity++;
+      } else {
+        state.items.push({ ...action.payload, quantity: 1 });
+      }
+    },
+    removeFromCart(state, action: PayloadAction<string>) {
+      const ItemIndex = state.items.findIndex(
+        (item) => item.id === action.payload
+      );
+      if (ItemIndex >= 0) {
+        state.items.splice(ItemIndex, 1);
+      } else {
+        state.items[ItemIndex].quantity--;
+      }
+    },
+  },
+});
