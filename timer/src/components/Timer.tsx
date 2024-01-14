c8cbfcc2c15305a071ce7cd56a1138c69a9417b3
gangbeomsu <kbs21c1987@gmail.com>
2024-01-14 15:26:17 +0900

    전체 stop과 start 기능 추가, 및 버그 수정

--- timer/src/components/Timer.tsx
+++ timer/src/components/Timer.tsx
@@ -1,26 +1,40 @@
 import { useEffect, useRef, useState } from 'react';
 import Container from './Ui/Container.tsx';
-import { Timer as TimerProps } from './store/timers-context.tsx';
+import {
+  Timer as TimerProps,
+  useTimersContext,
+} from './store/timers-context.tsx';
 
 export default function Timer({ name, duration }: TimerProps) {
   const interval = useRef<number | null>(null);
   const [remainingTime, setRemainingTime] = useState(duration * 1000);
+  const { isRunning } = useTimersContext();
 
   if (remainingTime <= 0 && interval.current) {
     clearInterval(interval.current);
   }
 
   useEffect(() => {
-    const timer = setInterval(() => {
-      setRemainingTime((prevTime) => prevTime - 50);
-    }, 50);
+    let timer: number;
+    if (isRunning) {
+      timer = setInterval(() => {
+        setRemainingTime((prevTime) => {
+          if (prevTime <= 0) {
+            return prevTime; // 0보다 작아지면 안되므로 0을 반환
+          }
+          return prevTime - 50;
+        });
+      }, 50);
 
-    interval.current = timer; // interval.current에 timer를 할당
+      interval.current = timer; // interval.current에 timer를 할당
+    } else if (!isRunning && interval.current) {
+      clearInterval(interval.current);
+    }
 
     return () => {
       clearInterval(timer);
     };
-  }, []);
+  }, [isRunning]);
 
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
 
